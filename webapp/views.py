ed8eea150c2e44bf2e2854b6059ef5aec7442c99
kittuov <kittuov@gmail.com>
2015-11-21 22:07:52 +0530

    updated login page

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.middleware.csrf import get_token
 from django.shortcuts import render_to_response
 
 
 def index(request):
+    get_token(request)
     return render_to_response('index.html')
 
 
